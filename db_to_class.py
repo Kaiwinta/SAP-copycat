37346eed2ac4bd8d5b8d0d286779c4603b86ce87
Kaiwinta <pradouratitouan@proton.me>
2023-08-19 21:33:55 +0200

    Update db_to_class.py

--- db_to_class.py
+++ db_to_class.py
@@ -2,6 +2,11 @@ import dbPart, class_part
 
 
 def creating_order(ref : int):
+    """Create an instance of and order
+
+    Args:
+        ref (int): the ref of the packages, scanned by the worker
+    """
     result = dbPart.creating_package(ref)
     packagelist = dbPart.charging_package(ref)
     print(packagelist)
