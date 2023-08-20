e7b5a3b10926b1d6194edb59f8d78cfdc41f0c45
Kaiwinta <pradouratitouan@proton.me>
2023-08-20 19:09:50 +0200

    data adding

--- generation random.py
+++ generation random.py
@@ -1,12 +1,29 @@
 from random import randint,shuffle
-liste = list(range(1,301))
+
+def generate_order():
+    liste = list(range(1,301))
 
 
-for i in range(300):
-    liste.append(randint(1,300))
+    for i in range(300):
+        liste.append(randint(1,300))
 
-shuffle(liste)
+    shuffle(liste)
 
 
-for i in liste:
-    print(i)
+    for i in liste:
+        print(i)
+
+def generate_size():
+    liste =[]
+    for i in range(600):
+        liste.append(randint(1,50))
+    for i in liste:
+        print(i)
+
+
+
+def generate_false():
+    for i in range(600):
+        print(False)
+
+generate_false()
